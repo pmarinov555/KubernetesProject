2c1f84c793c4e58fdb3d0d92156b51c639ef26c4
pmarinov555 <pmarinov555@gmail.com>
2024-12-18 20:48:33 +0000

    update

--- EKSCluster/backend.tf
+++ EKSCluster/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "XX" # CHANGE WITH YOUR S3 BUCKET
+    bucket = "pavel-kubernetes-project-v1" # CHANGE WITH YOUR S3 BUCKET
     key    = "K8inEKS/terraform.tfstate"
     region = "eu-west-2"
   }
